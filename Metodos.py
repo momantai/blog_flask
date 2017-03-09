ecac6069576cbf8c58e580eae9fe9dbffa5cb855
momantai <megaoz100@hotmail.com>
2017-03-09 13:35:40 -0600

    Se integro una interfaz más (aun por terminar) area de publicación completa, area para realizar un comentario y area de comentarios, con errores esteticos (con el footer).

--- Metodos.py
+++ Metodos.py
@@ -9,5 +9,9 @@ app= Flask(__name__)
 def principal():
     return render_template('Index.html')
 
+@app.route('/publicacion')
+def publ():
+	return render_template('Publicacion.html')
+
 if __name__=='__main__':
     app.run(debug=True, port=5000)
