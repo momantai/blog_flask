324e99a1653595f0c3a0670addf9d12f1bc8c27e
Jose_Armando <armando567@hotmail.com>
2017-03-13 17:53:21 -0600

    Conexion
    Se agrego el archivo conexion para poder insertar las publicaciones a la base de datos.
    En el archivo Metodos.py se agrego el codigo para basarnos al imprimir las distintas publicaciones y se agrego al index.html otro FOR para imprimir lo mismo que en el archivo python

--- Metodos.py
+++ Metodos.py
@@ -1,4 +1,4 @@
-
+import conexion
 from flask import Flask
 from flask import render_template
 
@@ -7,10 +7,19 @@ app= Flask(__name__)
 
 @app.route('/')
 def principal():
-    return render_template('Index.html')
+    conectar=conexion.conexion
+    sql="SELECT * FROM publicacion"
+    conectar.execute(sql)
+    resutado=conectar.fetchall()
+    lista=[]
+    for registro in resutado:
+       idpublicacion=registro[0]
+       cuerpo=registro[1]
+       lista.append(cuerpo)
+    return render_template('Index.html',lista=lista)
 
 @app.route('/publicacion')
-def publ():
+def publlicacion():
 	return render_template('Publicacion.html')
 
 if __name__=='__main__':
--- conexion.py
+++ conexion.py
@@ -6,17 +6,3 @@ def insertar():
     sql="INSERT INTO publicacion (cuerpo) VALUES ('adiis');"
     conexion.execute(sql)
     datos.commit()
-
-def imprecionparametros():
-    sql="SELECT * FROM publicacion WHERE idpublicacion='3'"
-    conexion.execute(sql)
-    resutado=conexion.fetchall()
-    for registro in resutado:
-        idpublicacion=registro[0]
-        cuerpo=registro[1]
-
-    print(cuerpo)
-
-
-
-imprecionparametros()
