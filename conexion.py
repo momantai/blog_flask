8fafd9bcfd958fc848be1bf0206d6419702fc14d
Jose_Armando <armando567@hotmail.com>
2017-03-21 16:37:57 -0600

    Categorias y submenus.
    Se modifico el color de la parte superior de el apartado de controles.
    Se cambiaron las categorias de tabla a forma de lista y  se agregaron submenus para las distintas categorias.

--- conexion.py
+++ conexion.py
@@ -1,8 +1,3 @@
 import MySQLdb
 datos=MySQLdb.connect("mysql2.paris1.alwaysdata.com","133991","momantai","momantaiter_blogflask")
 conexion=datos.cursor()
-
-def insertar():
-    sql="INSERT INTO publicacion (cuerpo) VALUES ('adiis');"
-    conexion.execute(sql)
-    datos.commit()
