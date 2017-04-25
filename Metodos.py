21b47ea64fe3db6c0e04c2f5858b5d65dde6bf94
momantai <megaoz100@hotmail.com>
2017-04-25 11:10:47 -0500

    Se modifico la forma de conectarse a la base de datos.

--- Metodos.py
+++ Metodos.py
@@ -1,7 +1,9 @@
-import conexion
+#import conexion
 from flask import Flask, request, flash, redirect, url_for
 from flask import render_template
 from flask import session, escape
+
+from flask_mysqldb import MySQL
 
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 photos = UploadSet('photos', IMAGES)
@@ -10,16 +12,23 @@ photos = UploadSet('photos', IMAGES)
 app= Flask(__name__)
 app.secret_key = "super secret key"
 
+app.config['MYSQL_HOST'] = "mysql2.paris1.alwaysdata.com"
+app.config['MYSQL_USER'] = "133991"
+app.config['MYSQL_PASSWORD'] = "momantai"
+app.config['MYSQL_DB'] = "momantaiter_blogflask"
+
+mysql = MySQL(app)
+
 #Configuración para cargar imagenes
 app.config['UPLOADED_PHOTOS_DEST'] = "static/imagenes"
 configure_uploads(app, photos)
-
-conectar=conexion.conexion
 
 
 #Area de direcciones o URL----------------------------------------------------------------------------------------------------------------------
 @app.route('/', methods=['POST', 'GET'])
 def principal():
+    conectar = mysql.connection.cursor()
+    
     xy = ""
     conectar.execute("SELECT * FROM publicacion ORDER BY id DESC limit 10")
     resutado=conectar.fetchall()
@@ -30,6 +39,8 @@ def principal():
 #Ruta de direcciones para entrar al perfil de algún usuario con su nombre de usuario de manera "+nameUser"
 @app.route('/+<user>', methods=['POST', 'GET'])
 def usuario(user):
+    conectar = mysql.connection.cursor()
+
     #PD. Aun no hace nada por que aun se esta haciendo el html, pero ya se comprobo que funciona la url.
     conectar.execute("SELECT idUser, user, correo FROM users WHERE user = (%s);", [user])
     resultado = conectar.fetchall()
@@ -42,6 +53,8 @@ def usuario(user):
 
 @app.route('/<categoria>/<subcategoria>')
 def categorias(categoria, subcategoria):
+    conectar = mysql.connection.cursor()
+
     conectar.execute("SELECT * FROM publicacion WHERE Categoria = (%s) and SubCategoria = (%s);", [[categoria], [subcategoria]])
     resultado = conectar.fetchall()
 
@@ -52,6 +65,8 @@ def categorias(categoria, subcategoria):
 
 @app.route('/<categoria>')
 def categoria(categoria):
+    conectar = mysql.connection.cursor()
+
     conectar.execute("SELECT * FROM publicacion WHERE Categoria = (%s);", [categoria])
     resultado = conectar.fetchall()
 
@@ -62,6 +77,8 @@ def categoria(categoria):
 
 @app.route('/publicacion')
 def publlicacion():
+    conectar = mysql.connection.cursor()
+
     idpublicacion = request.args.get('ID')#se recibe el parametro de la url de index.html para hacer la consulta y mostrar el contenido en la ventana.
     conectar.execute("SELECT * FROM publicacion WHERE id=(%s)" % idpublicacion)
     resultado=conectar.fetchall()
@@ -76,6 +93,8 @@ def publlicacion():
 
 @app.route('/publicar', methods=['POST', 'GET'])
 def publica():
+    conectar = mysql.connection.cursor()
+
     xy = ""
     if 'username' in session:
         xy = escape(session['username'])
@@ -86,6 +105,8 @@ def publica():
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+    conectar = mysql.connection.cursor()
+
     #Aquí obtenemos el archivo por metodo POST y es almacenado en la carpeta configurada para almacenar las imagenes.
     #También recogemos el valor de la ruta para ser alcacenada en base de datos.
     f = photos.save(request.files['file'])
@@ -117,6 +138,8 @@ def add_entry():
 
 @app.route('/addcoment', methods=['POST'])
 def add_comentario():
+    conectar = mysql.connection.cursor()
+
     idpublicacion=request.args.get('ID')
     conectar.execute("INSERT INTO comentarios(comentario,idpublicacion) values (%s, %s)",[request.form['comentario'],idpublicacion])
     conexion.datos.commit()
@@ -137,6 +160,8 @@ def a():
 
 @app.route('/loginIn', methods=['POST'])
 def loginIn():
+    conectar = mysql.connection.cursor()
+
     verr = 0
     #Obtiene los datos enviados desde el formulario de login.
     a = request.form['username']
@@ -166,6 +191,8 @@ def registrar():
 
 @app.route('/registerOn', methods=['POST'])
 def registrarOn():
+    conectar = mysql.connection.cursor()
+
     a = request.form['nombre']
     b = request.form['apellido']
     c = request.form['usuario']
