0a3d4bbcd8b304f53d2642ae487c6139f7d45b2d
momantai <megaoz100@hotmail.com>
2017-05-04 00:23:36 -0500

    Se acomodaron detlles menores.

--- Metodos.py
+++ Metodos.py
@@ -1,8 +1,8 @@
-#import conexion
 from flask import Flask, request, flash, redirect, url_for
 from flask import render_template
 from flask import session, escape
 import time
+import string
 from flask_mysqldb import MySQL
 import encriptacion
 from flask_uploads import UploadSet, configure_uploads, IMAGES
@@ -32,9 +32,9 @@ def principal():
     conectar = mysql.connection.cursor()
     xy = ""
     #conectar.execute("SELECT * FROM Publicacion ORDER BY idPublicacion DESC limit 10")
-    conectar.execute("""SELECT idPublicacion, titulo, cuerpo, portada,Nombre, Ape_pat, user, fechaPublicacion
-        FROM Publicacion INNER JOIN Usuario ON idUsuario = Usuario_idUsuario
-        INNER JOIN Usuario_datos ON idUsuario_datos = Usuario_datos_idUsuario_datos
+    conectar.execute("""SELECT idPublicacion, titulo, cuerpo, portada,Nombre, Ape_pat, user, fechaPublicacion 
+        FROM Publicacion INNER JOIN momantaiter_blogflask.Usuario ON Usuario.idUsuario = Publicacion.Usuario_idUsuario 
+        INNER JOIN momantaiter_blogflask.Usuario_datos ON Usuario_datos.idUsuario_datos = Usuario.Usuario_datos_idUsuario_datos 
         ORDER BY idPublicacion DESC limit 10""")
     resutado=conectar.fetchall()
     if 'username' in session: #Verifica si hay un usuario en sesion
@@ -48,9 +48,9 @@ def principal():
 def categorias(categoria, subcategoria):
     conectar = mysql.connection.cursor()
 
-    conectar.execute("""SELECT idPublicacion, titulo, cuerpo, portada,Nombre, Ape_pat, user, fechaPublicacion
-        FROM Publicacion INNER JOIN Usuario ON idUsuario = Usuario_idUsuario
-        INNER JOIN Usuario_datos ON idUsuario_datos = Usuario_datos_idUsuario_datos
+    conectar.execute("""SELECT idPublicacion, titulo, cuerpo, portada,Nombre, Ape_pat, user, fechaPublicacion 
+        FROM Publicacion INNER JOIN momantaiter_blogflask.Usuario ON Usuario.idUsuario = Publicacion.Usuario_idUsuario 
+        INNER JOIN momantaiter_blogflask.Usuario_datos ON Usuario_datos.idUsuario_datos = Usuario.Usuario_datos_idUsuario_datos 
         WHERE categoria = (%s) AND subCategoria = (%s) ORDER BY idPublicacion DESC""", [[categoria], [subcategoria]])
 
     resultado = conectar.fetchall()
@@ -64,9 +64,9 @@ def categorias(categoria, subcategoria):
 def categoria(categoria):
     conectar = mysql.connection.cursor()
 
-    conectar.execute("""SELECT idPublicacion, titulo, cuerpo, portada,Nombre, Ape_pat, user, fechaPublicacion
-        FROM Publicacion INNER JOIN Usuario ON idUsuario = Usuario_idUsuario
-        INNER JOIN Usuario_datos ON idUsuario_datos = Usuario_datos_idUsuario_datos
+    conectar.execute("""SELECT idPublicacion, titulo, cuerpo, portada,Nombre, Ape_pat, user, fechaPublicacion 
+        FROM Publicacion INNER JOIN momantaiter_blogflask.Usuario ON Usuario.idUsuario = Publicacion.Usuario_idUsuario 
+        INNER JOIN momantaiter_blogflask.Usuario_datos ON Usuario_datos.idUsuario_datos = Usuario.Usuario_datos_idUsuario_datos 
         WHERE categoria = (%s) ORDER BY idPublicacion DESC""", [categoria])
 
     resultado = conectar.fetchall()
@@ -84,7 +84,7 @@ def publlicacion():
     conectar.execute("SELECT * FROM Publicacion WHERE idPublicacion = (%s)" % idpublicacion)
     resultado=conectar.fetchall()
 
-    conectar.execute("SELECT Nombre, user, comentario FROM Comentario INNER JOIN Usuario ON idUsuario = Usuario_idUsuarioC INNER JOIN Usuario_datos ON idUsuario_datos = Usuario_datos_idUsuario_datos WHERE Publicacion_idPublicacionC = (%s)" %idpublicacion)
+    conectar.execute("SELECT Nombre, user, comentario FROM Comentario INNER JOIN Usuario ON Usuario.idUsuario = Comentario.Usuario_idUsuarioC INNER JOIN Usuario_datos ON Usuario_datos.idUsuario_datos = Usuario.Usuario_datos_idUsuario_datos WHERE Publicacion_idPublicacionC = (%s)" %idpublicacion)
     coments=conectar.fetchall()
     xy = ""
     if 'username' in session:
@@ -102,6 +102,53 @@ def publicar():
     else:
         return redirect(url_for("login"))
 
+@app.route('/editarPub')
+def editarP():
+    conectar = mysql.connection.cursor()
+    idP = request.args.get('ref')
+    if 'username' in session:
+        i = escape(session['id'])
+        conectar.execute("SELECT * FROM Publicacion WHERE idPublicacion = (%s) AND Usuario_idUsuario = (%s)", [[idP], [i]])
+        publicacion = conectar.fetchall()
+        print(publicacion)
+        if len(publicacion) != 0:
+            return render_template("modificarp.html", posteo = publicacion)
+        else:
+            return redirect("/")
+@app.route('/edit', methods=['POST'])
+def editP():
+    conectar = mysql.connection.cursor()
+    f = ""
+    subCat = ""
+
+    if request.form["categoria"]=="1":
+        subCat = request.form['cat1']
+    elif request.form["categoria"]=="2":
+        subCat = request.form['cat2']
+    elif request.form["categoria"]=="3":
+        subCat = request.form['cat3']
+    elif request.form["categoria"]=="4":
+        subCat = request.form['cat4']
+    elif request.form["categoria"]=="5":
+        subCat = request.form['cat5']
+    else:
+        subCat = ""
+
+    idU = escape(session['id'])
+    try:
+        f = photos.save(request.files['file'])
+
+        conectar.execute("""UPDATE Publicacion SET titulo = (%s), cuerpo = (%s), categoria = (%s), 
+            subCategoria = (%s), portada = (%s) WHERE idPublicacion = (%s) and  Usuario_idUsuario = (%s)
+            """, [[request.form['titulo']], [request.form['publicacion']], [request.form['categoria']], [subCat], [f], [request.form['xpubxid']], [idU]])
+        mysql.connection.commit()
+    except:
+        conectar.execute("""UPDATE Publicacion SET titulo = (%s), cuerpo = (%s), categoria = (%s), 
+            subCategoria = (%s) WHERE idPublicacion = (%s) and  Usuario_idUsuario = (%s)
+            """, [[request.form['titulo']], [request.form['publicacion']], [request.form['categoria']], [subCat], [request.form['xpubxid']], [idU]])
+        mysql.connection.commit()
+    return redirect("/publicacion?ID="+request.form['xpubxid'])
+        
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -125,9 +172,6 @@ def add_entry():
     else:
         subCat = ""
 
-    print(request.form['categoria'])
-    print(subCat)
-
     #PD. Se agrego la categoria y subcategoria.
     conectar.execute("INSERT INTO Publicacion(titulo, cuerpo, categoria,fechaPublicacion, subCategoria,Usuario_idUsuario, portada) values(%s, %s, %s, %s, %s,%s,%s)", [request.form['titulo'], request.form['publicacion'], request.form['categoria'],fecha, subCat,session['id'], [f]])
     mysql.connection.commit()
@@ -141,7 +185,7 @@ def add_entry():
 @app.route('/deletePub')
 def deletePub():
     conectar = mysql.connection.cursor()
-
+    
     id = request.args.get('pub')
     conectar.execute("DELETE FROM Comentario WHERE Publicacion_idPublicacionC = (%s)", [id]);
     mysql.connection.commit()
@@ -156,7 +200,9 @@ def add_comentario():
     fecha=time.strftime("%y/%m/%d")
     print(fecha)
     idpublicacion=request.args.get('ID')
-    conectar.execute("INSERT INTO Comentario(comentario,fechaComentario,Usuario_idUsuarioC, Publicacion_idPublicacionC) values (%s, %s,%s,%s)",[request.form['comentario'],fecha,session['id'],idpublicacion])
+    conectar.execute("""INSERT INTO Comentario(comentario,fechaComentario,Usuario_idUsuarioC, 
+        Publicacion_idPublicacionC) 
+        values (%s, %s,%s,%s)""",[request.form['comentario'],fecha,session['id'],idpublicacion])
     mysql.connection.commit()
     return redirect("/publicacion?ID=%s" %idpublicacion)
 
@@ -170,6 +216,8 @@ def login():
 def loginOut():
     return render_template('login.html', xerror=True)
 
+def a():
+    return "Hola"
 
 @app.route('/loginIn', methods=['POST'])
 def loginIn():
@@ -180,7 +228,7 @@ def loginIn():
     a = request.form['username']
     b = encriptar(request.form['passworde'])
     #Se conecta a la base de datos para verificar si el user y pass son correctos
-    conectar.execute("SELECT idUsuario,user FROM Usuario WHERE user=(%s) and password=(%s) or idUsuario=(%s) and password=(%s)", [a, b, a, b])
+    conectar.execute("SELECT idUsuario,user FROM Usuario WHERE user=(%s) and password=(%s) or correo=(%s) and password=(%s)", [a, b, a, b])
     respuesta = conectar.fetchone()
 
     if respuesta != None:
@@ -213,7 +261,7 @@ def registrarOn():
     c = request.form['usuario']
     d = request.form['correo']
     e = encriptar(request.form['contrasenia'])
-    x = "T"
+    x = b.split(" ")
 
     conectar.execute("SELECT user FROM Usuario WHERE user=(%s) or idUsuario=(%s)", [c, d])
     respuesta = conectar.fetchone()
@@ -221,20 +269,20 @@ def registrarOn():
 
 
     if respuesta == None:
-        conectar.execute("INSERT INTO Usuario_datos VALUES (NULL, %s, %s, %s);", [[a], [b], [x]])
+        conectar.execute("INSERT INTO Usuario_datos (idUsuario_datos,Nombre, Ape_pat, Ape_Mat, imagen_perfil, firma) VALUES (NULL, %s, %s, %s, 'defaultprofile.png','¡Soy nuevo aquí!');", [[a], [x[0]], [x[1]]])
         mysql.connection.commit()
         conectar.execute("SELECT MAX(idUsuario_datos) FROM Usuario_datos")
         idUsuario = conectar.fetchone()[0]
         print(idUsuario)
-        conectar.execute("INSERT INTO Usuario VALUES (NULL, %s, %s, %s);", [[c], [e], [idUsuario]])
+        conectar.execute("INSERT INTO Usuario VALUES (NULL, %s, %s, %s, %s);", [[c], [e], [idUsuario], [d]])
         mysql.connection.commit()
         return redirect("/login")
     else:
         print("Mal")
-        return "<b>mal</b>"
-
+        return "<b>Usuario o correo ya en uso.</b>"
 
 #Futura Area de Perfil de usuario :v
+
 @app.route('/+<user>', methods=['POST', 'GET'])
 def usuario(user):
     conectar = mysql.connection.cursor()
@@ -249,7 +297,6 @@ def usuario(user):
     if 'username' in session: #Verifica si hay un usuario en sesion
         xy = escape(session['username'])
         id=session['id']
-
     return render_template("user.html", sessionopen=xy, datos=resultado,publicaciones=publicaciones,id=id)
 
 @app.route('/Update-user')
@@ -271,6 +318,7 @@ def update_add():
     conectar.execute("UPDATE Usuario_datos SET nombre=%s,Ape_pat=%s,Ape_mat=%s WHERE idUsuario_datos=%s",[ nombre,apepat ,apemat ,id])
     mysql.connection.commit()
     return redirect('/+%s' %session['username'])
+
 #-------------------------------------------------------------------------------------------------------------------------------------------------
 if __name__=='__main__':
     app.run(debug=True, port=5000)
